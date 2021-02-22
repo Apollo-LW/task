016f9ef1344620cdba9f8ebd42507fe73ad54cfd
mrmoon <abuamaramfreelance@gmail.com>
2021-02-22 17:25:54 +0200

    commit handler documentation

--- src/main/java/com/apollo/task/constant/RoutingConstant.java
+++ src/main/java/com/apollo/task/constant/RoutingConstant.java
@@ -1,20 +1,20 @@
 package com.apollo.task.constant;
 
-public abstract class RoutingConstant {
+public interface RoutingConstant {
 
-    public static final String TASK_PATH = "/task";
-    public static final String USER_PATH = "/user";
-    public static final String TASK_ID = "taskId";
-    public static final String TASK_ID_PATH = "/{" + TASK_ID + "}";
-    public static final String OWNER_ID = "ownerId";
-    public static final String OWNER_ID_PATH = "/{" + OWNER_ID + "}";
-    public static final String USER_ID = "userId";
-    public static final String USER_ID_PATH = "/{" + USER_ID + "}";
-    public static final String TASK_TYPE = "taskType";
-    public static final String TASK_TYPE_PATH = "/{" + TASK_TYPE + "}";
-    public static final String TASK_STATUS = "taskStatus";
-    public static final String TASK_STATUS_PATH = "/{" + TASK_STATUS + "}";
-    public static final String GROUP_NAME = "groupName";
-    public static final String GROUP_NAME_PATH = "/{" + GROUP_NAME + "}";
-    public static final String TASK_USER_PATH = TASK_PATH + USER_PATH + USER_ID_PATH;
+    String TASK_PATH = "/task";
+    String USER_PATH = "/user";
+    String TASK_ID = "taskId";
+    String TASK_ID_PATH = "/{" + TASK_ID + "}";
+    String OWNER_ID = "ownerId";
+    String OWNER_ID_PATH = "/{" + OWNER_ID + "}";
+    String USER_ID = "userId";
+    String USER_ID_PATH = "/{" + USER_ID + "}";
+    String TASK_TYPE = "taskType";
+    String TASK_TYPE_PATH = "/{" + TASK_TYPE + "}";
+    String TASK_STATUS = "taskStatus";
+    String TASK_STATUS_PATH = "/{" + TASK_STATUS + "}";
+    String GROUP_NAME = "groupName";
+    String GROUP_NAME_PATH = "/{" + GROUP_NAME + "}";
+    String TASK_USER_PATH = TASK_PATH + USER_PATH + USER_ID_PATH;
 }
--- src/main/java/com/apollo/task/handler/TaskHandler.java
+++ src/main/java/com/apollo/task/handler/TaskHandler.java
@@ -11,30 +11,56 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+/**
+ * Main Task handler for the Task API
+ */
 @Component
 @RequiredArgsConstructor
 public class TaskHandler {
 
+    /**
+     * Task Service to handle data and event operations
+     */
     private final TaskService taskService;
 
+    /**
+     * Getting a certain task in it's final state using the {@link Task#getTaskId()}
+     *
+     * @param request the request that led for this method to execute, which contains the taskId in the path variable {@link RoutingConstant#TASK_ID}
+     *
+     * @return a {@link ServerResponse} with the task object in it, parsed in JSON. If the task was not found the response will indicate that
+     */
     public @NotNull Mono<ServerResponse> getTaskById(final ServerRequest request) {
         final String taskId = request.pathVariable(RoutingConstant.TASK_ID);
         final Mono<Task> taskMono = this.taskService.getTaskById(taskId).flatMap(Mono::justOrEmpty);
-        return ServerResponse
-                .ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(taskMono , Task.class);
+        return taskMono
+                .flatMap(task -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(task , Task.class))
+                .switchIfEmpty(ServerResponse.notFound().build());
     }
 
+    /**
+     * Create a new task event
+     *
+     * @param request the request that led for this method to execute, which contain the {@link Task} object in the body
+     *
+     * @return a {@link ServerResponse} with the created {@link Task} in the response. If the creation had an error in it the response will indicate that
+     */
     public @NotNull Mono<ServerResponse> createTask(final ServerRequest request) {
         final Mono<Task> taskMono = request.bodyToMono(Task.class);
         final Mono<Task> createdTask = this.taskService.saveTask(taskMono).flatMap(Mono::justOrEmpty);
-        return ServerResponse
-                .ok()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(createdTask , Task.class);
+        return createdTask
+                .flatMap(task -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(task , Task.class))
+                .switchIfEmpty(ServerResponse.badRequest().build());
     }
 
+    /**
+     * Updating the task event
+     *
+     * @param request the request that led for this method to execute, which contain the {@link Task} object in the body,
+     *                as well as one of the {@link Task#getTaskOwners()} in the path variable {@link RoutingConstant#OWNER_ID}
+     *
+     * @return a {@link ServerResponse} with the a boolean flag indicating if the update was successful or not.
+     */
     public @NotNull Mono<ServerResponse> updateTask(final ServerRequest request) {
         final Mono<Task> taskMono = request.bodyToMono(Task.class);
         final String ownerId = request.pathVariable(RoutingConstant.OWNER_ID);
@@ -45,6 +71,14 @@ public class TaskHandler {
                 .body(isUpdated , Boolean.class);
     }
 
+    /**
+     * Delete the task event
+     *
+     * @param request the request that led for this method to execute, which contain the {@link Task#getTaskId()} in the path variable {@link RoutingConstant#TASK_ID},
+     *                as well as one of the {@link Task#getTaskOwners()} in the path variable {@link RoutingConstant#OWNER_ID}
+     *
+     * @return a {@link ServerResponse} with a boolean flag indicating if the delete was successful or not
+     */
     public @NotNull Mono<ServerResponse> deleteTask(final ServerRequest request) {
         final String taskId = request.pathVariable(RoutingConstant.TASK_ID);
         final String ownerId = request.pathVariable(RoutingConstant.OWNER_ID);
